32235f4160e79b4ed0a13625b6e5d42da6915f53
nitinsharmamcl <sharmanitin.mcl@gmail.com>
2025-05-26 17:06:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'sql5.freesqldatabase.com',
-  user: process.env.DB_USER || 'sql5781213',
-  password: process.env.DB_PASSWORD || 'YxSfBRBzKw',
-  database: process.env.DB_NAME || 'sql5781213',
+  host: 'sql5.freesqldatabase.com',
+  user: 'sql5781213',
+  password: 'YxSfBRBzKw',
+  database: 'sql5781213',
 });
 
 module.exports = db; 
